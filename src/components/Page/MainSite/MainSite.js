c5916b9078c98bdbe9a4d88f223a93fb80b089cc
himanshu-futurionic11 <himanshu.futurionic@gmail.com>
2023-08-07 17:08:37 +0530

    info change

--- src/components/Page/MainSite/MainSite.js
+++ src/components/Page/MainSite/MainSite.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import Header from '../../Component/Header/Header'
-import Menu from '../../Component/Navbar/Menu'
-import HomePage from '../Home/HomePage'
+import React from "react";
+import Header from "../../Component/Header/Header";
+import Menu from "../../Component/Navbar/Menu";
+import HomePage from "../Home/HomePage";
+import { Button } from "flowbite-react";
+import { HiOutlineArrowLeft } from 'react-icons/hi';
 
 const MainSite = () => {
   return (
     <div>
-        {/* <Menu/> */}
-   {/* <Header/> */}
-   <HomePage/>
-    </div>
-  )
-}
+      {/* <Menu/> */}
+      {/* <Header/> */}
+      <div>
+        <Button className="ml-10 mt-10 -mb-10  " ><a className="flex" href="https://ssos.co.in/" ><HiOutlineArrowLeft className="mx-2 h-5 w-5" />Back to Main Site</a></Button>
+      </div>
 
-export default MainSite
+      <HomePage />
+    </div>
+  );
+};
+
+export default MainSite;
